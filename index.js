f59d5c4e34b7edb061044c05f7f8b8eeb032f1b0
Victoria Mei <vm930@hotmail.com>
2019-04-15 10:59:00 -0400

    finish creating js rendering with dummy data

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('hey');
+// console.log('hey');
+let url = 'http://localhost:3000/questions';
+fetch(url).then((resp) => resp.json()).then(function(questions) {
+	questions.forEach(function(question) {
+		displayQuestion(question);
+	});
+});
+
+function displayQuestion(question) {
+	const table = document.querySelector('.question');
+	table.innerHTML = `
+        <h1>${question.content}</h1>
+        `;
+}
