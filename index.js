e27e79c986ae8f19875079297417cb892f9e02b5
Victoria Mei <vm930@hotmail.com>
2019-04-15 15:03:53 -0400

    finish rendering questions to html and managed to add click event

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
-// console.log('hey');
-let url = 'http://localhost:3000/questions';
-fetch(url).then((resp) => resp.json()).then(function(questions) {
-	questions.forEach(function(question) {
-		displayQuestion(question);
-	});
+const urlBase = 'http://localhost:3000';
+const bodyTag = document.querySelector('body');
+const questionDiv = document.querySelector('#questionBlock');
+const answersDiv = document.querySelector('#answerOptionBlock');
+let questionsJson;
+
+//fetch data
+fetch(`${urlBase}/questions`).then((resp) => resp.json()).then(function(json) {
+	questionsJson = json;
+	displayQuestion(questionsJson[3]);
 });
 
 function displayQuestion(question) {
-	const table = document.querySelector('.question');
-	table.innerHTML = `
-        <h1>${question.content}</h1>
+	questionDiv.innerHTML = `
+        <h1>${question.content}</h1></br>
+        <div id = "answerOptionBlock">
+        <ul>
+        ${question.answers
+			.map(function(answer) {
+				return `<li data-iscorrect= "${answer.is_correct}">${answer.answer_content}</li>`;
+			})
+			.join('')}
+        </ul>
+        </div>
         `;
 }
+
+//add event listener
+bodyTag.addEventListener('click', function(e) {
+	if (e.target.dataset.iscorrect === 'true') {
+		//go to the next question
+	} else {
+		alert('try again!');
+	}
+}); //end of eventlistening
