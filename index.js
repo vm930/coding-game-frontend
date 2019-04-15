76471ac5005fc7dd445a3248fa68da8230e991e6
Kate Kim <katekim5809@gmail.com>
2019-04-15 16:05:25 -0400

    currentindex

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bodyTag = document.querySelector('body');
 const questionDiv = document.querySelector('#questionBlock');
 const answersDiv = document.querySelector('#answerOptionBlock');
 let questionsJson;
-let currentIndex = 2;
+let currentIndex = 10;
 
 //fetch data
 function fetchQuestion(currentIndex) {
@@ -37,8 +37,12 @@ bodyTag.addEventListener('click', function(e) {
 		//go to the next question
 		alert('SMORT!');
 		//update currentIndex
-		currentIndex++;
+		currentIndex ++;
+    // console.log(currentIndex);
 		fetchQuestion(currentIndex);
+    if(currentIndex == questionsJson.length && e.target.dataset.iscorrect === 'true'){
+      alert('congratulations🏆')
+    };
 	} else {
 		alert('try again!');
 	}
